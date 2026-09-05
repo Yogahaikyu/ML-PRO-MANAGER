a6086d88403805d22c3ece9282e2f5cacd20f2f9
Yogahaikyu <164295947+Yogahaikyu@users.noreply.github.com>
2026-09-05 03:38:52 +0100

    Update mpl-kh.js

--- data/mpl-kh.js
+++ data/mpl-kh.js
@@ -1,1 +1,659 @@
+const MPL_KH_2026 = {
+  id: "mpl-kh",
+  name: "MPL Cambodia",
+  season: 2026,
+  region: "KH",
 
+  teams: [
+
+    {
+      id: "cfu",
+      name: "CFU Gaming",
+      short: "CFU",
+
+      players: [
+        {
+          id: "cfu-wadu",
+          name: "Wadu",
+          role: "EXP",
+          nationality: "KH",
+          age: 21,
+          rating: 78,
+          potential: 86,
+          salary: 7000
+        },
+        {
+          id: "cfu-detective",
+          name: "Detective",
+          role: "Jungle",
+          nationality: "KH",
+          age: 22,
+          rating: 80,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "cfu-oppi",
+          name: "Oppi",
+          role: "Mid",
+          nationality: "KH",
+          age: 20,
+          rating: 79,
+          potential: 87,
+          salary: 7000
+        },
+        {
+          id: "cfu-xingg",
+          name: "Xingg",
+          role: "Gold",
+          nationality: "KH",
+          age: 21,
+          rating: 81,
+          potential: 89,
+          salary: 8000
+        },
+        {
+          id: "cfu-boxixixixi",
+          name: "Boxixixixi",
+          role: "Roam",
+          nationality: "KH",
+          age: 21,
+          rating: 78,
+          potential: 86,
+          salary: 7000
+        },
+        {
+          id: "cfu-zee",
+          name: "Zee",
+          role: "Mid",
+          nationality: "KH",
+          age: 20,
+          rating: 74,
+          potential: 84,
+          salary: 6000
+        }
+      ]
+    },
+
+
+    {
+      id: "team-max",
+      name: "Team Max",
+      short: "MAX",
+
+      players: [
+        {
+          id: "max-kingaries",
+          name: "King Aries",
+          role: "EXP",
+          nationality: "KH",
+          age: 22,
+          rating: 82,
+          potential: 88,
+          salary: 9000
+        },
+        {
+          id: "max-sakzuuu",
+          name: "Sakzuuu",
+          role: "Jungle",
+          nationality: "KH",
+          age: 21,
+          rating: 83,
+          potential: 90,
+          salary: 9000
+        },
+        {
+          id: "max-biimothekey",
+          name: "BiiMoTheKey",
+          role: "Mid",
+          nationality: "KH",
+          age: 20,
+          rating: 81,
+          potential: 89,
+          salary: 8000
+        },
+        {
+          id: "max-lay",
+          name: "Lay",
+          role: "Gold",
+          nationality: "KH",
+          age: 22,
+          rating: 82,
+          potential: 88,
+          salary: 9000
+        },
+        {
+          id: "max-cypherr",
+          name: "Cypherr",
+          role: "Roam",
+          nationality: "KH",
+          age: 21,
+          rating: 80,
+          potential: 87,
+          salary: 8000
+        },
+        {
+          id: "max-feitian",
+          name: "Feitian",
+          role: "Jungle",
+          nationality: "KH",
+          age: 20,
+          rating: 75,
+          potential: 84,
+          salary: 6000
+        },
+        {
+          id: "max-daazz",
+          name: "DaaZz",
+          role: "Roam",
+          nationality: "KH",
+          age: 20,
+          rating: 74,
+          potential: 83,
+          salary: 6000
+        }
+      ]
+    },
+
+
+    {
+      id: "galaxy-legends",
+      name: "Galaxy Legends",
+      short: "GL",
+
+      players: [
+        {
+          id: "gl-louis",
+          name: "Louis",
+          role: "EXP",
+          nationality: "PH",
+          age: 21,
+          rating: 81,
+          potential: 89,
+          salary: 8000
+        },
+        {
+          id: "gl-linnn",
+          name: "Linnn",
+          role: "Jungle",
+          nationality: "KH",
+          age: 20,
+          rating: 82,
+          potential: 90,
+          salary: 8000
+        },
+        {
+          id: "gl-ccat",
+          name: "C Cat",
+          role: "Mid",
+          nationality: "KH",
+          age: 21,
+          rating: 80,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "gl-wickyyy",
+          name: "Wickyyy",
+          role: "Gold",
+          nationality: "KH",
+          age: 21,
+          rating: 82,
+          potential: 89,
+          salary: 9000
+        },
+        {
+          id: "gl-cadenza",
+          name: "CADENZA",
+          role: "Roam",
+          nationality: "PH",
+          age: 24,
+          rating: 85,
+          potential: 88,
+          salary: 11000
+        },
+        {
+          id: "gl-tyar",
+          name: "Tyar",
+          role: "Mid",
+          nationality: "KH",
+          age: 20,
+          rating: 74,
+          potential: 84,
+          salary: 6000
+        }
+      ]
+    },
+
+
+    {
+      id: "valhalla",
+      name: "Valhalla",
+      short: "VAL",
+
+      players: [
+        {
+          id: "val-heads",
+          name: "HEADS",
+          role: "EXP",
+          nationality: "PH",
+          age: 23,
+          rating: 84,
+          potential: 88,
+          salary: 10000
+        },
+        {
+          id: "val-chma",
+          name: "CHMA",
+          role: "Jungle",
+          nationality: "KH",
+          age: 21,
+          rating: 81,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "val-naja",
+          name: "Naja",
+          role: "Mid",
+          nationality: "KH",
+          age: 21,
+          rating: 80,
+          potential: 87,
+          salary: 8000
+        },
+        {
+          id: "val-souuul",
+          name: "Souuul",
+          role: "Gold",
+          nationality: "PH",
+          age: 22,
+          rating: 83,
+          potential: 88,
+          salary: 9000
+        },
+        {
+          id: "val-secret",
+          name: "SECRET",
+          role: "Roam",
+          nationality: "KH",
+          age: 22,
+          rating: 82,
+          potential: 87,
+          salary: 9000
+        },
+        {
+          id: "val-nelson",
+          name: "Nelson",
+          role: "EXP",
+          nationality: "KH",
+          age: 20,
+          rating: 74,
+          potential: 84,
+          salary: 6000
+        },
+        {
+          id: "val-ar-ey",
+          name: "Ar Ey",
+          role: "EXP",
+          nationality: "PH",
+          age: 21,
+          rating: 76,
+          potential: 85,
+          salary: 6000
+        },
+        {
+          id: "val-dranz",
+          name: "DRanz",
+          role: "Gold",
+          nationality: "KH",
+          age: 20,
+          rating: 73,
+          potential: 84,
+          salary: 6000
+        }
+      ]
+    },
+
+
+    {
+      id: "pro-esports",
+      name: "PRO Esports",
+      short: "PRO",
+
+      players: [
+        {
+          id: "pro-sryimfelix",
+          name: "SryImFelix",
+          role: "EXP",
+          nationality: "KH",
+          age: 21,
+          rating: 81,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "pro-aezu",
+          name: "Aezu",
+          role: "Jungle",
+          nationality: "PH",
+          age: 20,
+          rating: 82,
+          potential: 90,
+          salary: 9000
+        },
+        {
+          id: "pro-saa",
+          name: "SAA",
+          role: "Mid",
+          nationality: "KH",
+          age: 21,
+          rating: 79,
+          potential: 87,
+          salary: 7000
+        },
+        {
+          id: "pro-kouzen",
+          name: "Kouzen",
+          role: "Gold",
+          nationality: "PH",
+          age: 21,
+          rating: 82,
+          potential: 89,
+          salary: 9000
+        },
+        {
+          id: "pro-d7",
+          name: "D7",
+          role: "Roam",
+          nationality: "KH",
+          age: 22,
+          rating: 80,
+          potential: 87,
+          salary: 8000
+        },
+        {
+          id: "pro-tuzuugod",
+          name: "TuzuuGod",
+          role: "Jungle",
+          nationality: "KH",
+          age: 20,
+          rating: 75,
+          potential: 85,
+          salary: 6000
+        },
+        {
+          id: "pro-netherlord",
+          name: "NetherLord",
+          role: "Jungle",
+          nationality: "KH",
+          age: 20,
+          rating: 74,
+          potential: 84,
+          salary: 6000
+        },
+        {
+          id: "pro-rungod",
+          name: "RunGod",
+          role: "Gold",
+          nationality: "KH",
+          age: 20,
+          rating: 74,
+          potential: 84,
+          salary: 6000
+        }
+      ]
+    },
+
+
+    {
+      id: "duck-rice",
+      name: "Duck Rice Esports",
+      short: "DUCK",
+
+      players: [
+        {
+          id: "duck-boom",
+          name: "Boom",
+          role: "EXP",
+          nationality: "KH",
+          age: 21,
+          rating: 79,
+          potential: 87,
+          salary: 7000
+        },
+        {
+          id: "duck-darkside",
+          name: "Darkside",
+          role: "Jungle",
+          nationality: "KH",
+          age: 21,
+          rating: 80,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "duck-sticcc",
+          name: "Sticcc",
+          role: "Mid",
+          nationality: "KH",
+          age: 20,
+          rating: 79,
+          potential: 87,
+          salary: 7000
+        },
+        {
+          id: "duck-vinzen21",
+          name: "Vinzen21",
+          role: "Gold",
+          nationality: "KH",
+          age: 21,
+          rating: 81,
+          potential: 89,
+          salary: 8000
+        },
+        {
+          id: "duck-kramgod",
+          name: "Kram God",
+          role: "Roam",
+          nationality: "PH",
+          age: 21,
+          rating: 80,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "duck-aiden",
+          name: "Aiden",
+          role: "EXP",
+          nationality: "KH",
+          age: 20,
+          rating: 74,
+          potential: 84,
+          salary: 6000
+        },
+        {
+          id: "duck-drafict",
+          name: "Drafict",
+          role: "Mid",
+          nationality: "KH",
+          age: 20,
+          rating: 73,
+          potential: 84,
+          salary: 6000
+        },
+        {
+          id: "duck-yami",
+          name: "Yami",
+          role: "Roam",
+          nationality: "KH",
+          age: 20,
+          rating: 73,
+          potential: 84,
+          salary: 6000
+        }
+      ]
+    },
+
+
+    {
+      id: "team-flash-kh",
+      name: "Team Flash KH",
+      short: "FL",
+
+      players: [
+        {
+          id: "flash-ding",
+          name: "Ding",
+          role: "EXP",
+          nationality: "PH",
+          age: 22,
+          rating: 84,
+          potential: 89,
+          salary: 10000
+        },
+        {
+          id: "flash-nabloyow",
+          name: "Nabloyow",
+          role: "Jungle",
+          nationality: "PH",
+          age: 21,
+          rating: 82,
+          potential: 89,
+          salary: 9000
+        },
+        {
+          id: "flash-imyouratm",
+          name: "ImYourATM",
+          role: "Mid",
+          nationality: "KH",
+          age: 21,
+          rating: 81,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "flash-kaishi",
+          name: "Kaishi",
+          role: "Gold",
+          nationality: "KH",
+          age: 20,
+          rating: 80,
+          potential: 89,
+          salary: 8000
+        },
+        {
+          id: "flash-tyyy",
+          name: "Tyyy",
+          role: "Roam",
+          nationality: "KH",
+          age: 21,
+          rating: 79,
+          potential: 87,
+          salary: 7000
+        },
+        {
+          id: "flash-bunlong",
+          name: "Bun Long",
+          role: "EXP",
+          nationality: "KH",
+          age: 20,
+          rating: 73,
+          potential: 83,
+          salary: 6000
+        },
+        {
+          id: "flash-chhe",
+          name: "Chhe",
+          role: "Jungle",
+          nationality: "KH",
+          age: 20,
+          rating: 73,
+          potential: 84,
+          salary: 6000
+        },
+        {
+          id: "flash-kurru",
+          name: "Kurru",
+          role: "Gold",
+          nationality: "KH",
+          age: 20,
+          rating: 72,
+          potential: 83,
+          salary: 6000
+        }
+      ]
+    },
+
+
+    {
+      id: "see-you-soon",
+      name: "See You Soon",
+      short: "SYS",
+
+      players: [
+        {
+          id: "sys-shadoww",
+          name: "Shadoww",
+          role: "EXP",
+          nationality: "KH",
+          age: 22,
+          rating: 83,
+          potential: 88,
+          salary: 10000
+        },
+        {
+          id: "sys-kayn",
+          name: "Kayn",
+          role: "Jungle",
+          nationality: "PH",
+          age: 20,
+          rating: 83,
+          potential: 91,
+          salary: 10000
+        },
+        {
+          id: "sys-acyra",
+          name: "ACYRA",
+          role: "Mid",
+          nationality: "KH",
+          age: 21,
+          rating: 81,
+          potential: 88,
+          salary: 8000
+        },
+        {
+          id: "sys-aonzo",
+          name: "Aonzo",
+          role: "Gold",
+          nationality: "KH",
+          age: 20,
+          rating: 80,
+          potential: 89,
+          salary: 8000
+        },
+        {
+          id: "sys-pp",
+          name: "PP",
+          role: "Roam",
+          nationality: "KH",
+          age: 21,
+          rating: 79,
+          potential: 87,
+          salary: 7000
+        },
+        {
+          id: "sys-donut",
+          name: "Donut",
+          role: "Gold",
+          nationality: "PH",
+          age: 21,
+          rating: 78,
+          potential: 87,
+          salary: 7000
+        }
+      ]
+    }
+
+  ]
+};
