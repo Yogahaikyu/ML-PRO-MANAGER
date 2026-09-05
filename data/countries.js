d8de5ff572bb453dec4fd45d60444c3d80c82515
Yogahaikyu <164295947+Yogahaikyu@users.noreply.github.com>
2026-09-05 03:32:50 +0100

    Create countries.js

---
+++ data/countries.js
@@ -0,0 +1,38 @@
+const countries = [
+  {
+    id: "id",
+    name: "Indonesia",
+    flag: "🇮🇩",
+    leagues: ["mpl-id"]
+  },
+  {
+    id: "ph",
+    name: "Philippines",
+    flag: "🇵🇭",
+    leagues: ["mpl-ph"]
+  },
+  {
+    id: "my",
+    name: "Malaysia",
+    flag: "🇲🇾",
+    leagues: ["mpl-my"]
+  },
+  {
+    id: "sg",
+    name: "Singapore",
+    flag: "🇸🇬",
+    leagues: ["mpl-sg"]
+  },
+  {
+    id: "kh",
+    name: "Cambodia",
+    flag: "🇰🇭",
+    leagues: ["mpl-kh"]
+  },
+  {
+    id: "br",
+    name: "Brazil",
+    flag: "🇧🇷",
+    leagues: ["mpl-br"]
+  }
+];
