0f1617bac6a689e83d1ca4ed242397b610e16871
Yogahaikyu <164295947+Yogahaikyu@users.noreply.github.com>
2026-09-05 10:16:58 +0100

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,63 @@
+/* =========================================================
+   MLBB PRO MANAGER
+   COUNTRIES / REGIONS
+   ========================================================= */
+
+const countries = [
+
+  {
+    id: "ID",
+    name: "Indonesia",
+    flag: "🇮🇩",
+    description: "Mobile Legends Indonesia"
+  },
+
+  {
+    id: "PH",
+    name: "Philippines",
+    flag: "🇵🇭",
+    description: "Mobile Legends Philippines"
+  },
+
+  {
+    id: "MY",
+    name: "Malaysia",
+    flag: "🇲🇾",
+    description: "Malaysia"
+  },
+
+  {
+    id: "SG",
+    name: "Singapore",
+    flag: "🇸🇬",
+    description: "Singapore"
+  },
+
+  {
+    id: "KH",
+    name: "Cambodia",
+    flag: "🇰🇭",
+    description: "Cambodia"
+  },
+
+  {
+    id: "MM",
+    name: "Myanmar",
+    flag: "🇲🇲",
+    description: "Myanmar"
+  },
+
+  {
+    id: "BR",
+    name: "Brazil",
+    flag: "🇧🇷",
+    description: "Brazil"
+  },
+
+  {
+    id: "LATAM",
+    name: "Latin America",
+    flag: "🌎",
+    description: "Latin America"
+  ]
+];
